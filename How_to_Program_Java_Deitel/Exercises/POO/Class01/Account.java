87d0f11f4e8e455e317e5eabf02acddee6d42ca1
Leonardo Bonifácio Vieira Santos <estudotrafego@outlook.com>
2023-12-16 17:16:07 -0300

    Another day of Study
    more light

--- How_to_Program_Java_Deitel/Exercises/POO/Class01/Account.java
+++ How_to_Program_Java_Deitel/Exercises/POO/Class01/Account.java
@@ -1,9 +1,36 @@
 package Exercises.POO.Class01;
 
-
-
 public class Account {
     private String name;
+    private double balance;
+
+
+    public Account(String name, double balance){// like __init__ of python, constructor of java
+        this.name = name;
+        if (balance > 0.0) {
+            this.balance = balance;
+        }
+    }
+
+    public void displayNameAndAccount(Account account, int initialOrFinal){
+        if (initialOrFinal == 1) {// display the initial values
+            System.out.println("Initial name is " + account.getName());
+            System.out.println("Initial balance is " + account.getBalance());
+        }
+        else if (initialOrFinal == 2) {// display the final values
+            System.out.println(account.getName() + " Balance: " + account.getBalance());
+        }
+    }
+
+    public void deposit(double depositAmount){
+        if (depositAmount > 0.0) {
+            this.balance += depositAmount;
+        }
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
 
 
     public void setName(String name){
--- How_to_Program_Java_Deitel/Exercises/POO/Class01/AccountTest.java
+++ How_to_Program_Java_Deitel/Exercises/POO/Class01/AccountTest.java
@@ -1,22 +1,32 @@
 package Exercises.POO.Class01;
 
-import java.util.Scanner;
 
 public class AccountTest {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        Account myAccount = new Account();
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        Account account1 = new Account("Jane green", 70.00);
+        Account account2 = new Account("John Blue", -7.49);
+        double depositAmount = 0.0;
+        
+        account1.displayNameAndAccount(account1, 1);
+        account2.displayNameAndAccount(account2, 1);
+        
 
-        System.out.println("Initial name is " + myAccount.getName());
+        System.out.println("Enter the deposit amount for account1: ");
+        depositAmount = input.nextDouble();
+        System.out.println("Adding " + depositAmount + " to account1");
+        account1.deposit(depositAmount);
 
-        System.out.print("Please enter the name: ");
-        String theName = input.nextLine();
+        
+        System.out.println("Enter the deposit amount for account2: ");
+        depositAmount = input.nextDouble();
+        System.out.println("Adding " + depositAmount + " to account2");
+        account2.deposit(depositAmount);
 
-        myAccount.setName(theName);
-        System.out.println();
-
-        System.out.println("Name in object myAccount is:" + myAccount.getName());
+        account1.displayNameAndAccount(account1, 2);
+        account2.displayNameAndAccount(account2, 2);
+        
 
     }
 }
