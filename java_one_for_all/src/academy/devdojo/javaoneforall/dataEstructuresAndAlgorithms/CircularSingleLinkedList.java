4810399be5d1871cdb96de86ce148c039e4ba8c4
Leonardo Bonifácio <estudotrafego@outlook.com>
2024-04-01 16:45:50 -0300

    More

---
+++ java_one_for_all/src/academy/devdojo/javaoneforall/dataEstructuresAndAlgorithms/CircularSingleLinkedList.java
@@ -0,0 +1,83 @@
+package academy.devdojo.javaoneforall.dataEstructuresAndAlgorithms;
+
+// In a circular Singly linked list, the last node of the list contains a pointer to the first node of the list. 
+// We can have circular singly linked list as well as circular doubly linked list.
+// We traverse a circular singly linked list until we reach the same node where we started. 
+// The circular singly liked list has no beginning and no ending. There is no null value present in the next part of any of the nodes.
+// Circular linked list are mostly used in task maintenance in operating systems.
+// There are many examples where circular linked list are being used in computer science including browser surfing where a record of pages visited
+//in the past by the user, is maintained in the form of circular linked lists and can be accessed again on clicking the previous button.
+public class CircularSingleLinkedList {
+
+    private class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    private ListNode last;
+    private int length;
+
+    public CircularSingleLinkedList() {
+        this.length = 0;
+        this.last = null;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public boolean isEmpty() {
+        return this.length == 0;
+    }
+
+    public void createCircularSingleLinkedList() {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(5);
+        ListNode third = new ListNode(10);
+        ListNode fourth = new ListNode(15);
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = first;
+        this.last = fourth;
+        length = 4;
+
+    }
+
+    public void displayAllElements() {
+        if (last == null)
+            return;
+        ListNode first = last.next;
+        while (first != last) {
+            System.out.print(first.data + " -> ");
+            first = first.next;
+        }
+        System.out.println(first.data);
+
+    }
+
+    public void insertFirst(int data) {
+        ListNode temp = new ListNode(data);
+        if (last == null) {
+            last = temp;
+        }
+        else{
+            temp.next = last.next;
+        }
+        last.next = temp;
+        length++;
+    }
+
+    public static void main(String[] args) {
+        CircularSingleLinkedList csll = new CircularSingleLinkedList();
+        csll.insertFirst(10);
+        csll.insertFirst(15);
+        csll.insertFirst(25);
+        csll.displayAllElements();
+        System.out.println(csll.getLength());
+    }
+}
---
+++ java_one_for_all/src/academy/devdojo/javaoneforall/dataEstructuresAndAlgorithms/DoublyLinkedList.java
@@ -0,0 +1,128 @@
+package academy.devdojo.javaoneforall.dataEstructuresAndAlgorithms;
+
+import java.util.NoSuchElementException;
+
+// Doubly linked list is a complex type of linked list in which a node contains a pointer to the previous as well as the next node in the sequence.
+// Therefore, in a doubly linked list, a node consists of three parts: node data, pointer to the next node in sequence (next pointer) ,
+// pointer to the previous node (previous pointer). In a singly linked list, we could traverse only in one direction,
+// because each node contains address of the next node and it doesn't have any record of its previous nodes. However, 
+// doubly linked list overcome this limitation of singly linked list. 
+// Due to the fact that, each node of the list contains the address of its previous node,
+// we can find all the details about the previous node as well by using the previous address stored inside the previous part of each node.
+public class DoublyLinkedList {// Outer Class DoublyLinkedList
+
+    private static class ListNode {// innerClass ListNode
+        private int data;
+        private ListNode previous;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.length == 0;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void insertFirst(int value) {
+        ListNode newNode = new ListNode(value);
+        if (isEmpty()) {
+            tail = newNode;
+        } else {
+            head.previous = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+        length++;
+    }
+
+    public void insertLast(int value) {
+        ListNode newNode = new ListNode(value);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.previous = tail;
+        }
+        tail = newNode;
+        length++;
+    }
+
+    public ListNode removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException("This DoublyLinkedList is Empty");
+        ListNode temp = head;
+        if (head == tail) {
+            tail = null;
+        } else {
+            head.next.previous = null;
+        }
+        head = head.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+    public ListNode removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException("Thsi DoublyLinkedList is Empty");
+        ListNode temp = tail;
+        if (head == tail) {
+            head = null;
+        } else {
+            tail.previous.next = null;
+        }
+        tail = tail.previous;
+        temp.previous = null;
+        length--;
+        return temp;
+    }
+
+    public void DisplayForwardDirection() {
+        if (head == null)
+            return;
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " --> ");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
+    public void DisplayBackwardDirection() {
+        if (tail == null)
+            return;
+        ListNode temp = tail;
+        while (temp != null) {
+            System.out.print(temp.data + " --> ");
+            temp = temp.previous;
+        }
+        System.out.println("Null");
+
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertFirst(1);
+        dll.insertLast(2);
+        dll.insertLast(3);
+        dll.removeFirst();
+        dll.removeLast();
+        dll.DisplayForwardDirection();
+    }
+}
